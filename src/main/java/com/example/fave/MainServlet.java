e0e543e4f3f0a7b25d23f9cc73cd7c97d6186aa4
yuki <yuuki.kikuchi.sys23@morijyobi.ac.jp>
2024-11-18 12:09:31 +0900

    色々修正

--- src/main/java/com/example/fave/MainServlet.java
+++ src/main/java/com/example/fave/MainServlet.java
@@ -4,11 +4,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
         @Override
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
                 if (request.getSession().getAttribute("user") == null) {
                         request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
                 } else {
