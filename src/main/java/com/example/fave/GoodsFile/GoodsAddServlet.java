7c85d0bbb261ec74351dd644d58bd9cf94347e3b
Imairi <m.imairi.sys23@morijyobi.ac.jp>
2024-12-12 12:16:08 +0900

    goods_add

--- src/main/java/com/example/fave/GoodsFile/GoodsAddServlet.java
+++ src/main/java/com/example/fave/GoodsFile/GoodsAddServlet.java
@@ -2,12 +2,15 @@ package com.example.fave.GoodsFile;
 
 import java.io.*;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet("/GoodsAdd")
 public class GoodsAddServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/GoodsFile/goods_add.jsp");
+        dispatcher.forward(request, response);
     }
 }
