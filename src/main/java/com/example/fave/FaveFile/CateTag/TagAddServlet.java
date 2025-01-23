988fc65a38da18f25a97e0a4d374cfbb62d6083b
yuki <yuuki.kikuchi.sys23@morijyobi.ac.jp>
2025-01-23 14:26:17 +0900

    tag修正のまきき

--- src/main/java/com/example/fave/FaveFile/CateTag/TagAddServlet.java
+++ src/main/java/com/example/fave/FaveFile/CateTag/TagAddServlet.java
@@ -1,39 +1,39 @@
-//package com.example.fave.FaveFile.CateTag;
-//
-//import java.io.*;
-//
-//import jakarta.servlet.http.*;
-//import jakarta.servlet.annotation.*;
-//import utils.Bean.userBean;
-//
-//@WebServlet("/TagAddServlet")
-//public class TagAddServlet extends HttpServlet {
-//    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        response.setContentType("text/html; charset=UTF-8");
-//        request.setCharacterEncoding("UTF-8");
-//        HttpSession session = request.getSession();
-//
-//        userBean user = (userBean) session.getAttribute("user");
-//        String log_id = user.getLog_id();
-//    }
-//
-//    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        response.setContentType("text/html; charset=UTF-8");
-//        request.setCharacterEncoding("UTF-8");
-//        HttpSession session = request.getSession();
-//
-//        userBean user = (userBean) session.getAttribute("user");
-//        String log_id = user.getLog_id();
-//
-//        String tag = request.getParameter("tag");
-//        int cate_id = Integer.parseInt(request.getParameter("cate_id"));
-//
-//
-//        utils.DAO.tagDAO.insertTag(cate_id,tag);
-//
-//        response.sendRedirect("relate");
-//    }
-//}
-//
-//
-//
+package com.example.fave.FaveFile.CateTag;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import utils.Bean.userBean;
+
+@WebServlet("/TagAddServlet")
+public class TagAddServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        HttpSession session = request.getSession();
+
+        userBean user = (userBean) session.getAttribute("user");
+        String log_id = user.getLog_id();
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        HttpSession session = request.getSession();
+
+        userBean user = (userBean) session.getAttribute("user");
+        String log_id = user.getLog_id();
+
+        String tag = request.getParameter("tag");
+        int cate_id = Integer.parseInt(request.getParameter("cate_id"));
+
+
+        utils.DAO.tagDAO.insertTag(cate_id,tag);
+
+        response.sendRedirect("relate");
+    }
+}
+
+
+
